d3684570b1232af3a7271e72b0ee1568bb543830
Wei Cui <ussf@umd.edu>
2024-05-25 06:22:30 -0400

    added a script to verify verify tflite model

--- verify_tflite.py
+++ verify_tflite.py
@@ -3,20 +3,23 @@ import numpy as np
 import tensorflow as tf
 
 # Load the TFLite model
-tflite_model_path = 'best-fp16.tflite'
+tflite_model_path = 'yolov5s-fp16.tflite'
 interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
 interpreter.allocate_tensors()
 
 # Get input and output details
 input_details = interpreter.get_input_details()
+print("input details", input_details)
 output_details = interpreter.get_output_details()
+print("output details", output_details)
 height, width = input_details[0]['shape'][1:3]
 
 # Load an example image for inference
-image_path = 'manikin2.jpg'
+image_path = 'data/images/manikin2.jpg'
 image = cv2.imread(image_path)
+print("image size", image.shape)
 print("width, height", width, height)
-image = cv2.resize(image, (width, height))
+# image = cv2.resize(image, (width, height))
 image = image / 255.0  # Normalize to [0, 1]
 image = np.expand_dims(image, axis=0)
 
@@ -28,12 +31,25 @@ interpreter.invoke()
 
 # Get the output tensor
 output_data = interpreter.get_tensor(output_details[0]['index'])
-
+print(output_data)
 # Post-process the output (adjust based on the structure of your YOLOv5 output)
 # For example, you might extract bounding box coordinates and class scores
 # and apply non-maximum suppression to get the final detections.
 
 # Display the results (adjust based on your post-processing)
+confidence_scores = output_data[:, :, 4]  # Confidence scores are in the 5th element of the last dimension
+print("scores", confidence_scores)
+
+class_probs = output_data[:, :, 5:]  # Class probabilities are from the 6th element onward
+print("class probs", class_probs)
+
+boxes = output_data[:, :, :4]  # Bounding boxes are in the first four elements of the last dimension
+print("boxes", boxes)
+
+# Assuming you want to get the maximum confidence class for each detection
+max_confidence_classes = np.argmax(class_probs, axis=-1)
+print("max conf class", max_confidence_classes)
+
 for detection in output_data[0]:
     confidence = detection[4]
 
